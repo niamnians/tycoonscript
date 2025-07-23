a5f38d151c8a60f7e5255a88079dac34be16b7a7
niamnians <jianterrenz123@gmail.com>
2025-07-23 11:48:12 +0800

    Update fast_frosted_hatch.lua

--- fast_frosted_hatch.lua
+++ fast_frosted_hatch.lua
@@ -1,37 +1,70 @@
--- Fast Frosted Egg Auto Hatch + Auto Bubble Collector
-
-local player = game.Players.LocalPlayer
-local RS = game:GetService("ReplicatedStorage")
-local ws = game:GetService("Workspace")
-
--- Check if triple hatch is available
-local eggRemote = RS:FindFirstChild("Network") and RS.Network:FindFirstChild("EggOp")
-local triple = true
-
--- AUTO HATCH function
-local function hatchLoop()
-    while true do
-        pcall(function()
-            eggRemote:InvokeServer("Frosted Egg", triple)
-        end)
-        task.wait(0.1)
+game:GetService("ReplicatedStorage").NetworkRemoteFunction:InvokeServer(unpack(args))
     end
+end)
+
+btns:Seperator()
+
+btns:Toggle("Toggle Events",false, function(bool)
+    getgenv().eventscool = bool 
+
+    while eventscool do wait()
+        game.ReplicatedStorage.Assets.Modules.Is2xSpeedEnabled.Enabled.Value = true
+        wait()
+        game:GetService("ReplicatedStorage").Assets.Modules.Is2xLuckEnabled.Value  = true
+    end
+end)
+
+local btns = serv:Channel("Eggs")
+
+local eggs = {};
+for i,v in pairs(workspace.Eggs:GetChildren()) do
+    table.insert(eggs, v.Name)
 end
 
--- AUTO BUBBLE collect
-local function bubbleLoop()
-    while true do
-        for _, v in ipairs(ws:GetDescendants()) do
-            if v:IsA("TouchTransmitter") and v.Parent.Name:lower():find("bubble") then
-                firetouchinterest(player.Character.HumanoidRootPart, v.Parent, 0)
-                task.wait(0.05)
-                firetouchinterest(player.Character.HumanoidRootPart, v.Parent, 1)
-            end
-        end
-        task.wait(1)
-    end
-end
+btns:Seperator()
 
--- Start both loops
-spawn(hatchLoop)
-spawn(bubbleLoop)
+btns:Dropdown("Choose Egg", eggs, function(CurrentOption)
+    wait()
+    SelectedEgg = CurrentOption
+end)
+
+btns:Toggle("Open Selected Egg",false, function(bool)
+    getgenv().singleegg = bool 
+
+    while singleegg do wait()
+        local args = {
+            [1] = "PurchaseEgg",
+            [2] = (SelectedEgg), 
+        }
+        
+        game:GetService("ReplicatedStorage").NetworkRemoteEvent:FireServer(unpack(args))
+    end
+end)
+
+btns:Seperator()
+
+btns:Toggle("Triple Open Selected Egg",false, function(bool)
+    getgenv().tripleeggs = bool 
+
+    while tripleeggs do wait()
+        local args = {
+            [1] = "PurchaseEgg",
+            [2] = (SelectedEgg), 
+            [3] = "Multi"
+            }
+
+        game:GetService("ReplicatedStorage").NetworkRemoteEvent:FireServer(unpack(args))
+    end
+end)
+
+btns:Seperator()
+
+btns:Button("Remove Egg Animation", function()
+    game:GetService("ReplicatedStorage").Assets.Eggs:Destroy()
+end)
+
+btns:Seperator()
+
+btns:Button("Stats Counter", function()
+    game:GetService("Players").LocalPlayer.PlayerGui.ScreenGui.MobileStats.Visible = true
+end)
